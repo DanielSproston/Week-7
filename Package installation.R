c689ac2785311417a355ca7989ebd2c68f26c60f
Daniel Sproston <dansproston@outlook.com>
2020-11-12 11:21:05 +0000

    with citation package

---
+++ Package installation.R
@@ -0,0 +1,11 @@
+install.packages("kableExtra")
+install.packages("flextable")
+install.packages("gt")
+devtools::install_github("ropensci/RefManageR")
+
+devtools::install_github("crsh/citr")
+force = TRUE
+library(citr)
+library(kableExtra)
+library(flextable)
+library(gt)
